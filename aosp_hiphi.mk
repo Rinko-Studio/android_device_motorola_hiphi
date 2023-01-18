7b8c22cb616621ebf62eb8c89433c91dd2daba19
LynnrinChan <lynnrin@lynnrin.moe>
2023-01-18 14:44:32 +0800

    hiphi: Enable more pixel stuff

--- aosp_hiphi.mk
+++ aosp_hiphi.mk
@@ -14,6 +14,8 @@ $(call inherit-product, device/motorola/hiphi/device.mk)
 # Inherit common configurations
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
